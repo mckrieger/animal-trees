347f9631c6c185929203a81e94ea072257396cdb
Mick Krieger <mckean.krieger@gmail.com>
2025-07-14 12:05:22 -0600

    remove nbsp

--- src/tree/index.js
+++ src/tree/index.js
@@ -4,13 +4,13 @@ import './index.css'
 export default function Tree() {
     return (
       <div className="tree">
-        mammals<br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cheetah <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bear <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lion <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ape <br/>
+        <p class="depth0">mammals</p>
+        <p class="depth1">cheetah </p>
+        <p class="depth1">bear </p>
+        <p class="depth2">lion </p>
+        <p class="depth2">dog </p>
+        <p class="depth3">elephant </p>
+        <p class="depth1">ape </p>
       </div>
     )
 }
