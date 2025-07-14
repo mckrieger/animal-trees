8617a6d01e47dbcc40bbab71876e956055d47161
Mick Krieger <mckean.krieger@gmail.com>
2025-07-14 13:24:57 -0600

    uses json to render animal tree

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+import data from './data.json';
 
 export default function Tree() {
     return (
-      <div className="tree">
-        <p class="depth0">mammals</p>
-        <p class="depth1">cheetah </p>
-        <p class="depth1">bear </p>
-        <p class="depth2">lion </p>
-        <p class="depth2">dog </p>
-        <p class="depth3">elephant </p>
-        <p class="depth1">ape </p>
-      </div>
-    )
+    <>
+        <TreeNode key={data.id} node={data} depth={0}/>
+    </>
+  );
 }
+
+const TreeNode = ({node, depth}) => {
+  const hasChildren = node.children && node.children.length > 0;
+  return (
+    <>
+      <p style={{marginLeft: `${depth * 50}px`}}>{node.name}</p>
+      {hasChildren && (
+        <>
+        {node.children.map((child) => (
+          <TreeNode key={child.id} node={child} depth={depth + 1}/>
+        ))}
+        </>
+      )}
+    </>
+  );
+};
